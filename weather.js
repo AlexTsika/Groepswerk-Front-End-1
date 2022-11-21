84ba6581cc6ff468175241c6433947c779f1f340
Remco Duckers <rempel.duckers@telenet.be>
2022-11-21 14:39:23 +0100

    data fetch url

--- weather.js
+++ weather.js
@@ -4,18 +4,18 @@ let session = JSON.parse(sessionStorage.getItem('infoObject'));
 if (local == null && session == null) {
     // if not redirect to login page
     window.location.href = 'login.html';
-};
-console.log(local, session);
+}
+console.log(local, session)
 // show name and bYear on page
 if (session == null) {
-    document.getElementById('name').innerHTML = local.name;
-    document.getElementById('bYear').innerHTML = local.bYear;
-    document.getElementById('location').innerHTML = local.location;
-};
-if (local == null) {
-    document.getElementById('name').innerHTML = session.name;
-    document.getElementById('bYear').innerHTML = session.bYear;
-    document.getElementById('location').innerHTML = session.location;
+    document.getElementById('name').innerHTML = local.name
+    document.getElementById('bYear').innerHTML = local.bYear
 };
 // data fetch API
 let url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+location+"?unitGroup=metric&include=current&key=P9J7ZGZ9SBGSZQTDBMT86WNN3&contentType=json'
+// fetch(url).then
+    fetch(url)
+           .then(response => response.json())
+           .then(data => {
+                console.log(data)
+           });               
