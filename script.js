e251df0dab4b8b8a0f255fa1e99044d95fbbdde5
Alexandros Tsikakis <Alexandros.Tsikakis@ymail.com>
2022-11-21 11:38:01 +0100

    inputfields html & script en doorverwijzing

---
+++ script.js
@@ -0,0 +1,29 @@
+// attach submit button to variable
+let submitButton = document.getElementById('submitButton');
+// assign event listener to login button
+submitButton.addEventListener('click', function () {
+    let name = document.getElementById('name').value;
+    let bDate = document.getElementById('bDate').value;
+    let location = document.getElementById('location').value;
+    let checkbox = document.getElementById('rememberMe').checked;
+    console.log(name, bDate, location, checkbox);
+    // create new object to send to server
+    let infoObject = {
+        name: name,
+        bDate: bDate,
+        location: location,
+        rememberMe: checkbox
+    };
+    console.log(infoObject);
+    // doorverwijzing
+    if (checkbox) {
+        // if checkbox is checked, save infoObject to local storage
+        localStorage.setItem('infoObject', JSON.stringify(infoObject));
+        // redirect to weather.html page
+        window.location.href = 'weather.html';
+    } else {
+        sessionStorage.setItem('infoObject', JSON.stringify(infoObject));
+        // redirect to weather.html page
+        window.location.href = 'weather.html';
+    }
+});
