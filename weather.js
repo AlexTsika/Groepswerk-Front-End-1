f85b45c60c76d23a11a94014fe77afff4119ba2e
Alexandros Tsikakis <Alexandros.Tsikakis@ymail.com>
2022-11-21 14:38:09 +0100

    weather.html changes

--- weather.js
+++ weather.js
@@ -4,13 +4,18 @@ let session = JSON.parse(sessionStorage.getItem('infoObject'));
 if (local == null && session == null) {
     // if not redirect to login page
     window.location.href = 'login.html';
-}
-console.log(local, session)
+};
+console.log(local, session);
 // show name and bYear on page
 if (session == null) {
-    document.getElementById('name').innerHTML = local.name
-    document.getElementById('bYear').innerHTML = local.bYear
+    document.getElementById('name').innerHTML = local.name;
+    document.getElementById('bYear').innerHTML = local.bYear;
+    document.getElementById('location').innerHTML = local.location;
+};
+if (local == null) {
+    document.getElementById('name').innerHTML = session.name;
+    document.getElementById('bYear').innerHTML = session.bYear;
+    document.getElementById('location').innerHTML = session.location;
 };
 // data fetch API
 let url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+location+"?unitGroup=metric&include=current&key=P9J7ZGZ9SBGSZQTDBMT86WNN3&contentType=json'
-// fetch(url).then
